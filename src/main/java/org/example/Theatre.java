ce6c10ee977f5796a6e7716fb1e3844b03abddab
Raymond <raymond.yiu@gmail.com>
2023-02-21 00:29:35 +0000

    add theatre.json

--- src/main/java/org/example/Theatre.java
+++ src/main/java/org/example/Theatre.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class Theatre implements IVenue {
@@ -13,7 +20,16 @@ public class Theatre implements IVenue {
         return name;
     }
     public void readJson(String filename) {
+        try {
+            Path path = Paths.get(
+                    ClassLoader.getSystemResource(filename).toURI());
+            Reader reader = Files.newBufferedReader(path);
+            CSVReader csvReader = new CSVReaderBuilder(reader).build();
+            allData = csvReader.readAll();
 
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
     public String reserveTicket(){
         return(seats.get(0));
